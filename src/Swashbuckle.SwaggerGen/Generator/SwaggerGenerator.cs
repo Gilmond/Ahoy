3f4c8ba21d8ac8256e76b28a5821a3a0b2217f84
Tommy Long <tommy.long@gilmond.com>
2016-08-23 14:56:47 +0100

    resolved options clone issue

--- src/Swashbuckle.SwaggerGen/Generator/SwaggerGenerator.cs
+++ src/Swashbuckle.SwaggerGen/Generator/SwaggerGenerator.cs
@@ -7,89 +7,89 @@ using Swashbuckle.Swagger.Model;
 
 namespace Swashbuckle.SwaggerGen.Generator
 {
-    public class SwaggerGenerator : ISwaggerProvider
-    {
-        private readonly IApiDescriptionGroupCollectionProvider _apiDescriptionsProvider;
-        private readonly ISchemaRegistryFactory _schemaRegistryFactory;
-        private readonly SwaggerGeneratorOptions _options;
+	public class SwaggerGenerator : ISwaggerProvider
+	{
+		private readonly IApiDescriptionGroupCollectionProvider _apiDescriptionsProvider;
+		private readonly ISchemaRegistryFactory _schemaRegistryFactory;
+		private readonly SwaggerGeneratorOptions _options;
 
-        public SwaggerGenerator(
-            IApiDescriptionGroupCollectionProvider apiDescriptionsProvider,
-            ISchemaRegistryFactory schemaRegistryFactory,
-            SwaggerGeneratorOptions options = null)
-        {
-            _apiDescriptionsProvider = apiDescriptionsProvider;
-            _schemaRegistryFactory = schemaRegistryFactory;
-            _options = options ?? new SwaggerGeneratorOptions();
-        }
+		public SwaggerGenerator(
+			IApiDescriptionGroupCollectionProvider apiDescriptionsProvider,
+			ISchemaRegistryFactory schemaRegistryFactory,
+			SwaggerGeneratorOptions options = null)
+		{
+			_apiDescriptionsProvider = apiDescriptionsProvider;
+			_schemaRegistryFactory = schemaRegistryFactory;
+			_options = options ?? new SwaggerGeneratorOptions();
+		}
 
-        public SwaggerDocument GetSwagger(
-            string apiVersion,
-            string host = null,
-            string basePath = null,
-            string[] schemes = null)
-        {
-            var schemaRegistry = _schemaRegistryFactory.Create();
+		public SwaggerDocument GetSwagger(
+			string apiVersion,
+			string host = null,
+			string basePath = null,
+			string[] schemes = null)
+		{
+			var schemaRegistry = _schemaRegistryFactory.Create();
 
-            var info = _options.ApiVersions.FirstOrDefault(v => v.Version == apiVersion);
-            if (info == null)
-                throw new UnknownApiVersion(apiVersion);
+			var info = _options.ApiVersions.FirstOrDefault(v => v.Version == apiVersion);
+			if (info == null)
+				throw new UnknownApiVersion(apiVersion);
 
-            var paths = GetApiDescriptionsFor(apiVersion)
-                .Where(apiDesc => !(_options.IgnoreObsoleteActions && apiDesc.IsObsolete()))
-                .OrderBy(_options.GroupNameSelector, _options.GroupNameComparer)
-                .GroupBy(apiDesc => apiDesc.RelativePathSansQueryString())
-                .ToDictionary(group => "/" + group.Key, group => CreatePathItem(group, schemaRegistry));
+			var paths = GetApiDescriptionsFor(apiVersion)
+				.Where(apiDesc => !(_options.IgnoreObsoleteActions && apiDesc.IsObsolete()))
+				.OrderBy(_options.GroupNameSelector, _options.GroupNameComparer)
+				.GroupBy(apiDesc => apiDesc.RelativePathSansQueryString())
+				.ToDictionary(group => "/" + group.Key, group => CreatePathItem(group, schemaRegistry));
 
-            var swaggerDoc = new SwaggerDocument
-            {
-                Info = info,
-                Host = host,
-                BasePath = basePath,
-                Schemes = schemes,
-                Paths = paths,
-                Definitions = schemaRegistry.Definitions,
-                SecurityDefinitions = _options.SecurityDefinitions
-            };
+			var swaggerDoc = new SwaggerDocument
+			{
+				Info = info,
+				Host = host,
+				BasePath = basePath,
+				Schemes = schemes,
+				Paths = paths,
+				Definitions = schemaRegistry.Definitions,
+				SecurityDefinitions = _options.SecurityDefinitions
+			};
 
-            var filterContext = new DocumentFilterContext(
-                _apiDescriptionsProvider.ApiDescriptionGroups,
-                schemaRegistry);
+			var filterContext = new DocumentFilterContext(
+				_apiDescriptionsProvider.ApiDescriptionGroups,
+				schemaRegistry);
 
-            foreach (var filter in _options.DocumentFilters)
-            {
-                filter.Apply(swaggerDoc, filterContext);
-            }
+			foreach (var filter in _options.DocumentFilters)
+			{
+				filter.Apply(swaggerDoc, filterContext);
+			}
 
-            return swaggerDoc;
-        }
+			return swaggerDoc;
+		}
 
-        private IEnumerable<ApiDescription> GetApiDescriptionsFor(string apiVersion)
-        {
-            var allDescriptions = _apiDescriptionsProvider.ApiDescriptionGroups.Items
-                .SelectMany(group => group.Items);
+		private IEnumerable<ApiDescription> GetApiDescriptionsFor(string apiVersion)
+		{
+			var allDescriptions = _apiDescriptionsProvider.ApiDescriptionGroups.Items
+				.SelectMany(group => group.Items);
 
-            return (_options.VersionSupportResolver == null)
-                ? allDescriptions
-                : allDescriptions.Where(apiDesc => _options.VersionSupportResolver(apiDesc, apiVersion));
-        }
+			return (_options.VersionSupportResolver == null)
+				? allDescriptions
+				: allDescriptions.Where(apiDesc => _options.VersionSupportResolver(apiDesc, apiVersion));
+		}
 
-        private PathItem CreatePathItem(IEnumerable<ApiDescription> apiDescriptions, ISchemaRegistry schemaRegistry)
-        {
-            var pathItem = new PathItem();
+		private PathItem CreatePathItem(IEnumerable<ApiDescription> apiDescriptions, ISchemaRegistry schemaRegistry)
+		{
+			var pathItem = new PathItem();
 
-            // Group further by http method
-            var perMethodGrouping = apiDescriptions
-                .GroupBy(apiDesc => apiDesc.HttpMethod);
-                
-            foreach (var group in perMethodGrouping)
-            {
-                var httpMethod = group.Key;
+			// Group further by http method
+			var perMethodGrouping = apiDescriptions
+				.GroupBy(apiDesc => apiDesc.HttpMethod);
+				
+			foreach (var group in perMethodGrouping)
+			{
+				var httpMethod = group.Key;
 
-                if (string.IsNullOrWhiteSpace(httpMethod))
-                    throw new NotSupportedException(string.Format(
-                        "Unbounded HTTP verbs for path '{0}'. Are you missing an HttpMethodAttribute?",
-                        group.First().RelativePathSansQueryString()));
+				if (string.IsNullOrWhiteSpace(httpMethod))
+					throw new NotSupportedException(string.Format(
+						"Unbounded HTTP verbs for path '{0}'. Are you missing an HttpMethodAttribute?",
+						group.First().RelativePathSansQueryString()));
 
 				ApiDescription apiDescription;
 				if (group.Count() > 1)
@@ -97,128 +97,128 @@ namespace Swashbuckle.SwaggerGen.Generator
 				else
 					apiDescription = group.Single();
 
-                switch (httpMethod)
-                {
-                    case "GET":
-                        pathItem.Get = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "PUT":
-                        pathItem.Put = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "POST":
-                        pathItem.Post = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "DELETE":
-                        pathItem.Delete = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "OPTIONS":
-                        pathItem.Options = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "HEAD":
-                        pathItem.Head = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "PATCH":
-                        pathItem.Patch = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                }
-            }
+				switch (httpMethod)
+				{
+					case "GET":
+						pathItem.Get = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "PUT":
+						pathItem.Put = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "POST":
+						pathItem.Post = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "DELETE":
+						pathItem.Delete = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "OPTIONS":
+						pathItem.Options = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "HEAD":
+						pathItem.Head = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "PATCH":
+						pathItem.Patch = CreateOperation(apiDescription, schemaRegistry);
+						break;
+				}
+			}
 
-            return pathItem;
-        }
+			return pathItem;
+		}
 
-        private Operation CreateOperation(ApiDescription apiDescription, ISchemaRegistry schemaRegistry)
-        {
-            var groupName = _options.GroupNameSelector(apiDescription);
+		private Operation CreateOperation(ApiDescription apiDescription, ISchemaRegistry schemaRegistry)
+		{
+			var groupName = _options.GroupNameSelector(apiDescription);
 
-            var parameters = apiDescription.ParameterDescriptions
-                .Where(paramDesc => paramDesc.Source.IsFromRequest)
-                .Select(paramDesc => CreateParameter(paramDesc, schemaRegistry))
-                .ToList();
+			var parameters = apiDescription.ParameterDescriptions
+				.Where(paramDesc => paramDesc.Source.IsFromRequest)
+				.Select(paramDesc => CreateParameter(paramDesc, schemaRegistry))
+				.ToList();
 
-            var responses = apiDescription.SupportedResponseTypes
-                .DefaultIfEmpty(new ApiResponseType { StatusCode = 200 })
-                .ToDictionary(
-                    apiResponseType => apiResponseType.StatusCode.ToString(),
-                    apiResponseType => CreateResponse(apiResponseType, schemaRegistry)
-                 );
+			var responses = apiDescription.SupportedResponseTypes
+				.DefaultIfEmpty(new ApiResponseType { StatusCode = 200 })
+				.ToDictionary(
+					apiResponseType => apiResponseType.StatusCode.ToString(),
+					apiResponseType => CreateResponse(apiResponseType, schemaRegistry)
+				 );
 
-            var operation = new Operation
-            {
-                Tags = (groupName != null) ? new[] { groupName } : null,
-                OperationId = apiDescription.FriendlyId(),
-                Consumes = apiDescription.SupportedRequestMediaTypes().ToList(),
-                Produces = apiDescription.SupportedResponseMediaTypes().ToList(),
-                Parameters = parameters.Any() ? parameters : null, // parameters can be null but not empty
-                Responses = responses,
-                Deprecated = apiDescription.IsObsolete()
-            };
+			var operation = new Operation
+			{
+				Tags = (groupName != null) ? new[] { groupName } : null,
+				OperationId = apiDescription.FriendlyId(),
+				Consumes = apiDescription.SupportedRequestMediaTypes().ToList(),
+				Produces = apiDescription.SupportedResponseMediaTypes().ToList(),
+				Parameters = parameters.Any() ? parameters : null, // parameters can be null but not empty
+				Responses = responses,
+				Deprecated = apiDescription.IsObsolete()
+			};
 
-            var filterContext = new OperationFilterContext(apiDescription, schemaRegistry);
-            foreach (var filter in _options.OperationFilters)
-            {
-                filter.Apply(operation, filterContext);
-            }
+			var filterContext = new OperationFilterContext(apiDescription, schemaRegistry);
+			foreach (var filter in _options.OperationFilters)
+			{
+				filter.Apply(operation, filterContext);
+			}
 
-            return operation;
-        }
+			return operation;
+		}
 
-        private IParameter CreateParameter(ApiParameterDescription paramDesc, ISchemaRegistry schemaRegistry)
-        {
-            var source = paramDesc.Source.Id.ToLower();
-            var schema = (paramDesc.Type == null) ? null : schemaRegistry.GetOrRegister(paramDesc.Type);
+		private IParameter CreateParameter(ApiParameterDescription paramDesc, ISchemaRegistry schemaRegistry)
+		{
+			var source = paramDesc.Source.Id.ToLower();
+			var schema = (paramDesc.Type == null) ? null : schemaRegistry.GetOrRegister(paramDesc.Type);
 
-            if (source == "body")
-            {
-                return new BodyParameter
-                {
-                    Name = paramDesc.Name,
-                    In = source,
-                    Schema = schema
-                };
-            }
-            else
-            {
-                var nonBodyParam = new NonBodyParameter
-                {
-                    Name = paramDesc.Name,
-                    In = source,
-                    Required = (source == "path")
-                };
+			if (source == "body")
+			{
+				return new BodyParameter
+				{
+					Name = paramDesc.Name,
+					In = source,
+					Schema = schema
+				};
+			}
+			else
+			{
+				var nonBodyParam = new NonBodyParameter
+				{
+					Name = paramDesc.Name,
+					In = source,
+					Required = (source == "path")
+				};
 
-                if (schema == null)
-                    nonBodyParam.Type = "string";
-                else
-                    nonBodyParam.PopulateFrom(schema);
+				if (schema == null)
+					nonBodyParam.Type = "string";
+				else
+					nonBodyParam.PopulateFrom(schema);
 
-                if (nonBodyParam.Type == "array")
-                    nonBodyParam.CollectionFormat = "multi";
+				if (nonBodyParam.Type == "array")
+					nonBodyParam.CollectionFormat = "multi";
 
-                return nonBodyParam;
-            }
-        }
+				return nonBodyParam;
+			}
+		}
 
-        private Response CreateResponse(ApiResponseType apiResponseType, ISchemaRegistry schemaRegistry)
-        {
-            var description = ResponseDescriptionMap
-                .FirstOrDefault((entry) => Regex.IsMatch(apiResponseType.StatusCode.ToString(), entry.Key))
-                .Value;
+		private Response CreateResponse(ApiResponseType apiResponseType, ISchemaRegistry schemaRegistry)
+		{
+			var description = ResponseDescriptionMap
+				.FirstOrDefault((entry) => Regex.IsMatch(apiResponseType.StatusCode.ToString(), entry.Key))
+				.Value;
 
-            return new Response
-            {
-                Description = description,
-                Schema = (apiResponseType.Type != null && apiResponseType.Type != typeof(void))
-                    ? schemaRegistry.GetOrRegister(apiResponseType.Type)
-                    : null
-            };
-        }
+			return new Response
+			{
+				Description = description,
+				Schema = (apiResponseType.Type != null && apiResponseType.Type != typeof(void))
+					? schemaRegistry.GetOrRegister(apiResponseType.Type)
+					: null
+			};
+		}
 
-        private static readonly Dictionary<string, string> ResponseDescriptionMap = new Dictionary<string, string>
-        {
-            { "1\\d{2}", "Information" },
-            { "2\\d{2}", "Success" },
-            { "3\\d{2}", "Redirect" },
-            { "4\\d{2}", "Client Error" },
-            { "5\\d{2}", "Server Error" }
-        };
-    }
+		private static readonly Dictionary<string, string> ResponseDescriptionMap = new Dictionary<string, string>
+		{
+			{ "1\\d{2}", "Information" },
+			{ "2\\d{2}", "Success" },
+			{ "3\\d{2}", "Redirect" },
+			{ "4\\d{2}", "Client Error" },
+			{ "5\\d{2}", "Server Error" }
+		};
+	}
 }
